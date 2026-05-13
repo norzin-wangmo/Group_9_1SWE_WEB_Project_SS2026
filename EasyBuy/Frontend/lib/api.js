8b8843e4a90b07f30df17f511f10b4ce4d6c1ae9
Kinley Wangmo <kineywamo@Kinleys-MacBook-Pro.local>
2026-05-13 21:25:44 +0600

    Added lib

---
+++ EasyBuy/Frontend/lib/api.js
@@ -0,0 +1,13 @@
+const API_URL = "http://localhost:5000/api";
+
+export async function loginUser(data) {
+  const response = await fetch(`${API_URL}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  return response.json();
+}
